393bf6e410f8b140d1045ad647d0826c4e75d2a3
youcanshine <youcanshine@foxmail.com>
2020-12-12 22:10:57 +0800

    cartline views

--- carts/views.py
+++ carts/views.py
@@ -15,4 +15,16 @@ class CartLineViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         cart, _ = Cart.objects.get_or_create(user=request.user)
-        
+        product_variant = get_object_or_404(
+            ProductVariant, pk=request.data.get('product_variant')
+        )
+        data = request.data.copy()
+        data['cart'] = cart.pk
+        data['unit_price'] = product_variant.base_price
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED,
+            headers=self.get_success_headers(serializer.data)
+        )
